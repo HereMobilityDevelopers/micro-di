7628837e5a7a1d77527742d33039d8388b9462d4
Natan Zalkin <natan.zalkin@me.com>
2020-04-05 17:18:04 +0300

    Return to umd format

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
   output: [{
       file: pkg.main,
-      format: 'es',
+      name: "microdi",
+      format: "umd",
+      exports: "named",
       sourcemap: true
   }],
   plugins: [
